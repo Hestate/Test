f7a138087505b72130865b363e825af42701ea91
Hestate <hestate1337@gmail.com>
2022-07-13 19:29:59 +0300

    overflow_fix

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -25,188 +25,199 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
     final node = FocusScope.of(context);
+    
     return Scaffold(
       backgroundColor: AppColors.grey300,
       body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 50, 0, 5),
-                child: Text(
-                  'Welcome!',
-                  style: AppTypography.font56b,
-                ),
-              ),
-              Text(
-                'Sign up in your account!',
-                style: AppTypography.font28,
-              ),
-              const Spacer(flex: 1),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
-                child: Container(
-                  decoration: AppFieldBoxDecoration.fieldDecoration,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: TextFormField(
-                      controller: loginController,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Login',
+        child: LayoutBuilder(builder: (context, constraint) {
+          return Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraint.maxHeight),
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 50, 0, 5),
+                        child: Text(
+                          'Welcome!',
+                          style: AppTypography.font56b,
+                        ),
                       ),
-                      validator: (value) {
-                        return validateLogin(value!);
-                      },
-                      textInputAction: TextInputAction.next,
-                      onEditingComplete: () => node.nextFocus(),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
-                child: Container(
-                  decoration: AppFieldBoxDecoration.fieldDecoration,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: TextFormField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Email',
+                      Text(
+                        'Sign up in your account!',
+                        style: AppTypography.font28,
                       ),
-                      validator: (value) {
-                        return validateEmail(value!);
-                      },
-                      textInputAction: TextInputAction.next,
-                      onEditingComplete: () => node.nextFocus(),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
-                child: Container(
-                  decoration: AppFieldBoxDecoration.fieldDecoration,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: TextFormField(
-                      controller: passController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        errorMaxLines: 3,
-                        border: InputBorder.none,
-                        hintText: 'Password',
-                      ),
-                      validator: (value) {
-                        return validatePassword(value!);
-                      },
-                      textInputAction: TextInputAction.next,
-                      onEditingComplete: () => node.nextFocus(),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
-                child: Container(
-                  decoration: AppFieldBoxDecoration.fieldDecoration,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: TextFormField(
-                      controller: confirmPassController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Confirm Password',
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Password is required.";
-                        }
-                        if (value != passController.value.text) {
-                          return "Password do not match.";
-                        }
-                        return null;
-                      },
-                      onEditingComplete: () => node.unfocus(),
-                    ),
-                  ),
-                ),
-              ),
-              const Spacer(flex: 1),
-              GestureDetector(
-                onTap: () async {
-                  if (_formKey.currentState!.validate()) {
-                    debugPrint("Details are validated!!");
-                    debugPrint("Login: ${loginController.text}");
-                    debugPrint("Email: ${emailController.text}");
-                    debugPrint("Password: ${passController.text}");
-                    var personObject = Person(
-                      loginController.text.toString(),
-                      emailController.text.toString(),
-                      passController.text.toString(),
-                    );
-                    PersonDatabaseHelper person = PersonDatabaseHelper();
-                    await person.initializeDatabase();
-                    await person.insertPerson(personObject);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("You're sucsefully registered!"),
-                      ),
-                    );
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
-                      ),
-                    );
-                  }
-                },
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
-                  child: Container(
-                    padding: const EdgeInsets.all(15),
-                    decoration: AppButtonBoxDecoration.buttonDecoration,
-                    child: Center(
-                      child: Text(
-                        'Sign Up',
-                        style: AppTypography.font22wb,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(25, 10, 25, 50),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Already registered? ',
-                      style: AppTypography.font18g,
-                    ),
-                    GestureDetector(
-                      onTap: (() {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: ((context) => const LoginScreen()),
+                      const Spacer(flex: 1),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+                        child: Container(
+                          decoration: AppFieldBoxDecoration.fieldDecoration,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: TextFormField(
+                              controller: loginController,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Login',
+                              ),
+                              validator: (value) {
+                                return validateLogin(value!);
+                              },
+                              textInputAction: TextInputAction.next,
+                              onEditingComplete: () => node.nextFocus(),
+                            ),
                           ),
-                        );
-                      }),
-                      child: Text(
-                        'Sign In now!',
-                        style: AppTypography.font18bb,
+                        ),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+                        child: Container(
+                          decoration: AppFieldBoxDecoration.fieldDecoration,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: TextFormField(
+                              controller: emailController,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Email',
+                              ),
+                              validator: (value) {
+                                return validateEmail(value!);
+                              },
+                              textInputAction: TextInputAction.next,
+                              onEditingComplete: () => node.nextFocus(),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+                        child: Container(
+                          decoration: AppFieldBoxDecoration.fieldDecoration,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: TextFormField(
+                              controller: passController,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                errorMaxLines: 3,
+                                border: InputBorder.none,
+                                hintText: 'Password',
+                              ),
+                              validator: (value) {
+                                return validatePassword(value!);
+                              },
+                              textInputAction: TextInputAction.next,
+                              onEditingComplete: () => node.nextFocus(),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+                        child: Container(
+                          decoration: AppFieldBoxDecoration.fieldDecoration,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: TextFormField(
+                              controller: confirmPassController,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Confirm Password',
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return "Password is required.";
+                                }
+                                if (value != passController.value.text) {
+                                  return "Password do not match.";
+                                }
+                                return null;
+                              },
+                              onEditingComplete: () => node.unfocus(),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Spacer(flex: 1),
+                      GestureDetector(
+                        onTap: () async {
+                          if (_formKey.currentState!.validate()) {
+                            debugPrint("Details are validated!!");
+                            debugPrint("Login: ${loginController.text}");
+                            debugPrint("Email: ${emailController.text}");
+                            debugPrint("Password: ${passController.text}");
+                            var personObject = Person(
+                              loginController.text.toString(),
+                              emailController.text.toString(),
+                              passController.text.toString(),
+                            );
+                            PersonDatabaseHelper person =
+                                PersonDatabaseHelper();
+                            await person.initializeDatabase();
+                            await person.insertPerson(personObject);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("You're sucsefully registered!"),
+                              ),
+                            );
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const LoginScreen(),
+                              ),
+                            );
+                          }
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+                          child: Container(
+                            padding: const EdgeInsets.all(15),
+                            decoration: AppButtonBoxDecoration.buttonDecoration,
+                            child: Center(
+                              child: Text(
+                                'Sign Up',
+                                style: AppTypography.font22wb,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(25, 10, 25, 50),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Already registered? ',
+                              style: AppTypography.font18g,
+                            ),
+                            GestureDetector(
+                              onTap: (() {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: ((context) => const LoginScreen()),
+                                  ),
+                                );
+                              }),
+                              child: Text(
+                                'Sign In now!',
+                                style: AppTypography.font18bb,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        }),
       ),
     );
   }
