79dae83d81dfe0e78d70e5235c1af9dd6ccb3533
Hestate <hestate1337@gmail.com>
2022-07-13 17:14:35 +0300

    profilePage_added_fields

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import '../utils/colors.dart';
+import '../utils/decorations.dart';
 import '../utils/fonts.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -12,20 +12,54 @@ class ProfilePage extends StatelessWidget {
       backgroundColor: AppColors.grey300,
       body: SafeArea(
         child: Center(
-          child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(Icons.person_outline, size: 200),
-                const SizedBox(height: 50),
-                Text(
-                  'Profile',
-                  style: AppTypography.font56b,
-                ),
-              ],
+          children: [
+            const Spacer(flex: 1),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 50, 0, 5),
+              child: Text(
+                'Hello',
+                style: AppTypography.font56b,
+              ),
             ),
-          ),
-        ),
+            Text(
+              'Username',
+              style: AppTypography.font20,
+            ),
+            const Spacer(flex: 2),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
+              child: Container(
+                decoration: AppFieldBoxDecoration.fieldDecoration,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: TextField(readOnly: true),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
+              child: Container(
+                decoration: AppFieldBoxDecoration.fieldDecoration,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: TextField(readOnly: true),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(25, 25, 25, 25),
+              child: Container(
+                decoration: AppFieldBoxDecoration.fieldDecoration,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: TextField(readOnly: true),
+                ),
+              ),
+            ),
+            const Spacer(flex: 2),
+          ],
+        )),
       ),
     );
   }
